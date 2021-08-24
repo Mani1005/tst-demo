404c2927c1936d37fe0f95c2ce3c2f78e5bce393
Mani1005 <87566437+Mani1005@users.noreply.github.com>
2021-08-24 10:24:04 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,1 +1,14 @@
-
+public class sample{
+  
+  public static void main(String[] args) {
+    
+    System.out.println("Hello One");
+    System.out.println("Hello Two");
+    System.out.println("Hello Three");
+    System.out.println("Hello Four");
+    System.out.println("Hello Five");
+    
+  }
+    
+} 
+  
